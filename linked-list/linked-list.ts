e7d15faaa3c645c1016d02f82bacd156927d3b0a
Felipe Marques <felipe.ferreira.marques@ibm.com>
2024-04-15 20:51:13 -0300

    feat: implements push into linked list

--- linked-list/linked-list.ts
+++ linked-list/linked-list.ts
@@ -2,10 +2,23 @@ export class LinkedList<T = any> {
     private head: Node<T>
     private tail: Node<T>
     private length: number
-    constructor(private readonly value: Node<T>) {
+    constructor(private readonly value: Node<T> | T) {
         this.head = value instanceof Node ? value : new Node<T>(value)
         this.tail = this.head
         this.length = 1
+    }
+
+    push(value: T): this {
+        const newNode = new Node(value)
+        if (this.length === 0) {
+            this.head = newNode
+            this.tail = newNode
+        } else {
+            this.tail.next = newNode
+            this.tail = newNode
+        }
+        this.length++
+        return this
     }
 }
 
