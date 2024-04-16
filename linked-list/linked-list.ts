a5c1f865ee4c7d5088ec86529bf998614b8033f9
Felipe Marques <felipe.ferreira.marques@ibm.com>
2024-04-15 21:33:58 -0300

    feat: implements pop into linked list

--- linked-list/linked-list.ts
+++ linked-list/linked-list.ts
@@ -1,6 +1,6 @@
 export class LinkedList<T = any> {
-    private head: Node<T>
-    private tail: Node<T>
+    private head: Node<T> | null
+    private tail: Node<T> | null
     private length: number
     constructor(private readonly value: Node<T> | T) {
         this.head = value instanceof Node ? value : new Node<T>(value)
@@ -14,11 +14,38 @@ export class LinkedList<T = any> {
             this.head = newNode
             this.tail = newNode
         } else {
-            this.tail.next = newNode
+            this.tail!.next = newNode
             this.tail = newNode
         }
         this.length++
         return this
+    }
+
+    pop(): Node {
+        if (this.length === 0) {
+            throw new Error('The list is empty')
+        }
+        if (this.length === 1) {
+            const temp = this.tail
+            this.emptyList()
+            return temp!
+        }
+        let temp = this.head
+        let pre = this.head
+        while (temp!.next) {
+            pre = temp
+            temp = temp!.next
+        }
+        this.tail = pre
+        this.tail!.next = null
+        this.length--
+        return temp!
+    }
+
+    private emptyList(): void {
+        this.head = null
+        this.tail = null
+        this.length = 0
     }
 }
 
