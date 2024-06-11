8e6296f125abdec65f8085bfb44b011f9e279e3d
Felipe Marques <flipmorsch@gmail.com>
2024-06-11 19:54:01 -0300

    feat: implements reverse into linked list

--- linked-list/linked-list.ts
+++ linked-list/linked-list.ts
@@ -118,6 +118,21 @@ export class LinkedList<T = any> {
         return temp
     }
 
+    reverse(): this {
+        let temp = this.head
+        this.head = this.tail
+        this.tail = temp
+        let next = temp.next
+        let prev = null
+        for (let i= 0; i < this.length; i++) {
+            next = temp.next
+            temp.next = prev
+            prev = temp
+            temp = next
+        }
+        return this
+    }
+
     private emptyList(): void {
         this.head = null
         this.tail = null
